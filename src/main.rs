ce6b846916ec312cee0e4d46f499e42bc8083747
Ziyi Yan <ziyi.yan@foxmail.com>
2021-01-24 00:46:59 +0800

    implement vga_buffer with a safe interface

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,16 @@
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello, world!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    panic!("HELP!");
 }
