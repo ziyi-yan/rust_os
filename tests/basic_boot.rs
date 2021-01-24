441c1c402faa69546e03e126c4f8813da3c3109f
Ziyi Yan <ziyi.yan@foxmail.com>
2021-01-25 01:45:08 +0800

    setup testing for kernel
    - serial port for printing to host
    - qemu exit signal port
    - custom test framework
    - lib.rs is an independent compilation unit from main.rs
    - #![cfg(...)] and #![cfg_attr(..., ...)] for conditional compilation

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,32 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+
 use rust_os::println;
+use rust_os::test_panic_handler;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    rust_os::test_panic_handler(info)
+    test_panic_handler(info);
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
